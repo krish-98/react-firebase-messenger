93c44dfabc1dff93e02a7137a3398c23587aa8b1
krish-98 <muralikrishnanm98@gmail.com>
2022-05-27 10:22:34 +0530

    Login page done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { Route, Routes } from "react-router-dom"
 import Navbar from "./components/Navbar"
-import Home from "./pages/Home"
-import Register from "./pages/Register"
 
 import "./App.css"
+
+// pages
+import Home from "./pages/Home"
+import Register from "./pages/Register"
+import Login from "./pages/Login"
 
 const App = () => {
   return (
@@ -12,6 +15,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </>
   )
