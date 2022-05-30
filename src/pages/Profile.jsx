7e12f6b4bc2d2ca3537b0657186b79320ea54b1b
krish-98 <muralikrishnanm98@gmail.com>
2022-05-30 14:22:12 +0530

    user profile created

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,40 @@
-import React from "react"
+import React, { useState } from "react"
+import Camera from "../components/svg/Camera"
+import Img from "../images/image1.jpg"
 
 const Profile = () => {
-  return <div>Profile</div>
+  const [img, setImg] = useState("")
+  console.log(img)
+
+  return (
+    <section>
+      <div className="profile_container">
+        <div className="img_container">
+          <img src={Img} alt="avatar" />
+          <div className="overlay">
+            <div>
+              <label htmlFor="photo">
+                <Camera />
+              </label>
+              <input
+                type="file"
+                accept="image/*"
+                style={{ display: "none" }}
+                id="photo"
+                onChange={(e) => setImg(e.target.files[0])}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="text_container">
+          <h3>Username</h3>
+          <p>User Email</p>
+          <hr />
+          <small>Joined On: ...</small>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default Profile
