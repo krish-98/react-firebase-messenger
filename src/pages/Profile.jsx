3f65c2c433031f1f6bba4c382e34d6122136755e
krish-98 <muralikrishnanm98@gmail.com>
2022-05-31 11:10:48 +0530

    profile image upload to firebase storage

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,16 +1,50 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import Camera from "../components/svg/Camera"
 import Img from "../images/image1.jpg"
+import { auth, db, storage } from "../firebase"
+import { ref, getDownloadURL, uploadBytes } from "firebase/storage"
+import { doc, getDoc, updateDoc } from "firebase/firestore"
 
 const Profile = () => {
   const [img, setImg] = useState("")
-  console.log(img)
+  const [user, setUser] = useState()
 
-  return (
+  useEffect(() => {
+    getDoc(doc(db, "users", auth.currentUser.uid)).then((docSnap) => {
+      if (docSnap.exists) {
+        setUser(docSnap.data())
+      }
+    })
+
+    if (img) {
+      const uploadImg = async () => {
+        const imgRef = ref(
+          storage,
+          `avatar/${new Date().getTime()} - ${img.name}`
+        )
+        try {
+          const snap = await uploadBytes(imgRef, img)
+          const url = await getDownloadURL(ref(storage, snap.ref.fullPath))
+
+          await updateDoc(doc(db, "users", auth.currentUser.uid), {
+            avatar: url,
+            avatarPath: snap.ref.fullPath,
+          })
+
+          setImg("")
+        } catch (error) {
+          console.log(error.msg)
+        }
+      }
+      uploadImg()
+    }
+  }, [img])
+
+  return user ? (
     <section>
       <div className="profile_container">
         <div className="img_container">
-          <img src={Img} alt="avatar" />
+          <img src={user.avatar || Img} alt="avatar" />
           <div className="overlay">
             <div>
               <label htmlFor="photo">
@@ -27,14 +61,14 @@ const Profile = () => {
           </div>
         </div>
         <div className="text_container">
-          <h3>Username</h3>
-          <p>User Email</p>
+          <h3>{user.name}</h3>
+          <p>{user.email}</p>
           <hr />
-          <small>Joined On: ...</small>
+          <small>Joined On: {user.createdAt.toDate().toDateString()}</small>
         </div>
       </div>
     </section>
-  )
+  ) : null
 }
 
 export default Profile
